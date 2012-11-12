jamiesternlicht/LTLMoP

commit 929b96e3c6dc069bbb79293144620eba3b6b2f0c

fixed a case where rfi.getFaces() returned a tuple instead of Point object

src/lib/regions.py
@@ -15,7 +15,7 @@
 
 # TODO: store regions with absolute coordinates, not x/y-pos + relative!!
 
-import os, sys
+import os, sys, copy
 import fileMethods
 import re, random, math
 import Polygon, Polygon.Utils, os
@@ -707,7 +707,7 @@ class Region:
         lastPt = None
         for pt in self.getPoints():
 
-            thisPt = (pt.x, pt.y)
+            thisPt = copy.deepcopy(pt)
 
             if lastPt != None:
                 yield tuple(sorted((lastPt, thisPt)))
@@ -722,7 +722,7 @@ class Region:
             for i,hole in enumerate(self.holeList):
                 lastPt = None
                 for pt in self.getPoints(hole_id=i):
-                    thisPt = (pt.x, pt.y)
+                    thisPt = copy.deepcopy(pt)
 
                     if lastPt != None:
                         yield tuple(sorted((lastPt, thisPt)))

commit 185f2769e23f25b2733a257f07dd4a6c7bd3b914

switch to using frozenset of Point objects for faces (fixes a problem introduced by 929b96e3); further reduce extent of wx dependency for regions; some motion controllers may need updating

src/lib/handlers/motionControl/heatController.py
@@ -87,29 +87,27 @@ class motionControlHandler:
         # Let's go get a controller!
 
         if last:
-            transFace = None
+            transFaceIdx = None
         else:
             # Find a face to go through
             # TODO: Account for non-determinacy?
             # For now, let's just choose the largest face available, because we are probably using a big clunky robot
-            max_magsq = 0
-            for tf in self.rfi.transitions[current][next]:
-                magsq = (tf[0].x - tf[1].x)**2 + (tf[0].y - tf[1].y)**2
-                if magsq > max_magsq:
-                    pt1, pt2 = tf
-                    max_magsq = magsq
-            
-            transFace = None
-            # Find the index of this face
             # TODO: Why don't we just store this as the index?
-            for i, face in enumerate([x for x in self.rfi.regions[current].getFaces()]):
-                # Account for both face orientations...
-                if (pt1 == face[0] and pt2 == face[1]) or (pt1 == face[1] and pt2 == face[0]):
-                    transFace = i
-                    break
-            
-            if transFace is None:
-                print "ERROR: Unable to find transition face between regions %s and %s.  Please check the decomposition (try viewing projectname_decomposed.regions in RegionEditor or a text editor)." % (self.rfi.regions[current].name, self.rfi.regions[next].name)
+            transFaceIdx = None
+            max_magsq = 0
+            for i, face in enumerate(self.rfi.regions[current_reg].getFaces()):
+                if face not in self.rfi.transitions[current_reg][next_reg]:
+                    continue
+
+                tf_pta, tf_ptb = face
+                tf_vector = tf_ptb - tf_pta
+                magsq = (tf_vector.x)**2 + (tf_vector.y)**2
+                if magsq > max_magsq:
+                    transFaceIdx = i
+                    max_magsq = magsq
+                
+            if transFaceIdx is None:
+                print "ERROR: Unable to find transition face between regions %s and %s.  Please check the decomposition (try viewing projectname_decomposed.regions in RegionEditor or a text editor)." % (self.rfi.regions[current_reg].name, self.rfi.regions[next_reg].name)
          
         # Transform the region vertices into real coordinates
         pointArray = [x for x in self.rfi.regions[current].getPoints()]
@@ -117,7 +115,7 @@ class motionControlHandler:
         vertices = mat(pointArray).T 
         
         # Get a controller function
-        controller = heatControllerHelper.getController(vertices, transFace, last)
+        controller = heatControllerHelper.getController(vertices, transFaceIdx, last)
 
         # Cache it in
         cache[current][next] = controller

src/lib/handlers/motionControl/vectorController.py
@@ -56,33 +56,31 @@ class motionControlHandler:
         vertices = mat(pointArray).T 
 
         if last:
-            transFace = None
+            transFaceIdx = None
         else:
             # Find a face to go through
             # TODO: Account for non-determinacy?
             # For now, let's just choose the largest face available, because we are probably using a big clunky robot
-            max_magsq = 0
-            for tf in self.rfi.transitions[current_reg][next_reg]:
-                magsq = (tf[0].x - tf[1].x)**2 + (tf[0].y - tf[1].y)**2
-                if magsq > max_magsq:
-                    pt1, pt2 = tf
-                    max_magsq = magsq
-            
-            transFace = None
-            # Find the index of this face
             # TODO: Why don't we just store this as the index?
-            for i, face in enumerate([x for x in self.rfi.regions[current_reg].getFaces()]):
-                # Account for both face orientations...
-                if (pt1 == face[0] and pt2 == face[1]) or (pt1 == face[1] and pt2 == face[0]):
-                    transFace = i
-                    break
-            
-            if transFace is None:
+            transFaceIdx = None
+            max_magsq = 0
+            for i, face in enumerate(self.rfi.regions[current_reg].getFaces()):
+                if face not in self.rfi.transitions[current_reg][next_reg]:
+                    continue
+
+                tf_pta, tf_ptb = face
+                tf_vector = tf_ptb - tf_pta
+                magsq = (tf_vector.x)**2 + (tf_vector.y)**2
+                if magsq > max_magsq:
+                    transFaceIdx = i
+                    max_magsq = magsq
+                
+            if transFaceIdx is None:
                 print "ERROR: Unable to find transition face between regions %s and %s.  Please check the decomposition (try viewing projectname_decomposed.regions in RegionEditor or a text editor)." % (self.rfi.regions[current_reg].name, self.rfi.regions[next_reg].name)
          
 
 		# Run algorithm to find a velocity vector (global frame) to take the robot to the next region
-        V = vectorControllerHelper.getController([pose[0], pose[1]], vertices, transFace)
+        V = vectorControllerHelper.getController([pose[0], pose[1]], vertices, transFaceIdx)
 
         # Pass this desired velocity on to the drive handler
         self.drive_handler.setVelocity(V[0], V[1], pose[2])

src/lib/mapRenderer.py
@@ -1,13 +1,25 @@
 import wx
 import sys, os
 from regions import *
-from itertools import chain
 
 #----------------------------------------------------------------------------
 
 class DrawableRegion(Region):
     """ Extends the Region class to allow drawing.
     """
+
+    def __init__(self, *args, **kwds):
+        super(DrawableRegion, self).__init__(*args, **kwds)
+
+    @classmethod
+    def fromRegion(cls, region):
+        # Create a new DrawableRegion object
+        obj = cls(region.type)
+    
+        # Copy over the data from the Region object
+        obj.setData(region.getData())
+
+        return obj
 
     # ============================
     # == Object Drawing Methods ==
@@ -21,7 +33,7 @@ class DrawableRegion(Region):
         """
 
         if self.name.lower() == "boundary":
-            dc.SetPen(wx.Pen(self.color, 3, wx.SOLID))
+            dc.SetPen(wx.Pen(wx.Colour(*self.color), 3, wx.SOLID))
             dc.SetBrush(wx.Brush(wx.Colour(self.color.Red(), self.color.Green(),
                          self.color.Blue(), 0), wx.TRANSPARENT))
         else:
@@ -31,7 +43,7 @@ class DrawableRegion(Region):
                 dc.SetBrush(wx.Brush(wx.Colour(newcolor.Red(), newcolor.Green(),
                              newcolor.Blue(), 128), wx.SOLID))
             else: 
-                dc.SetPen(wx.Pen(self.color, 1, wx.SOLID))
+                dc.SetPen(wx.Pen(wx.Colour(*self.color), 1, wx.SOLID))
                 dc.SetBrush(wx.Brush(wx.Colour(self.color.Red(), self.color.Green(),
                              self.color.Blue(), 128), wx.SOLID))
 
@@ -143,8 +155,7 @@ def drawMap(target, rfi, scaleToFit=True, drawLabels=True, highlightList=[], dee
         if isinstance(region, DrawableRegion):
             continue
 
-        obj = DrawableRegion(region.type)
-        obj.setData(region.getData())
+        obj = DrawableRegion.fromRegion(region)
         rfi.regions[i] = obj
         del region
 
@@ -215,8 +226,8 @@ def drawMap(target, rfi, scaleToFit=True, drawLabels=True, highlightList=[], dee
                 dc.SetBrush(wx.Brush(newcolor, wx.SOLID))
                 dc.SetPen(wx.Pen(newcolor, 1, wx.SOLID))
             else:
-                dc.SetBrush(wx.Brush(obj.color, wx.SOLID))
-                dc.SetPen(wx.Pen(obj.color, 1, wx.SOLID))
+                dc.SetBrush(wx.Brush(wx.Colour(*obj.color), wx.SOLID))
+                dc.SetPen(wx.Pen(wx.Colour(*obj.color), 1, wx.SOLID))
 
             center = obj.getCenter()
             if obj.name.lower() == "boundary":

src/lib/regions.py
@@ -20,111 +20,102 @@ import fileMethods
 import re, random, math
 import Polygon, Polygon.Utils, os
 import json
+from numbers import Number
 
 Polygon.setTolerance(0.01)
 
-if 'wx' in sys.modules:
-    print "wx is loaded, using wx objects for regions"
+class Point(object):
+    def __init__(self, x, y):
+        self.x = float(x)
+        self.y = float(y)
 
-    import wx
+    def __repr__(self):
+        return "Point(%f, %f)" % (self.x, self.y)
 
-    Point = wx.Point
-    Size = wx.Size
-    Color = wx.Colour
-else:
-    print "wx is NOT loaded, using ltlmop objects for regions"
-    from numbers import Number
+    def __add__(self, other):
+        if (isinstance(other, Point) or
+           (hasattr(other, "x") and hasattr(other, "y"))):
+            return Point(self.x+other.x, self.y+other.y)
+        else:
+            raise TypeError("Points can only be added to other points")
 
-    class Point(object):
-        def __init__(self, x, y):
-            self.x = float(x)
-            self.y = float(y)
+    def __mul__(self, other):
+        if isinstance(other, Number):
+            return Point(self.x*other, self.y*other)
+        else:
+            raise TypeError("Points can only multiplied by numbers")
 
-        def __str__(self):
-            return "(%f, %f)" % (self.x, self.y)
+    def __rmul__(self, *args, **kwds):
+        return self.__mul__(*args, **kwds)
 
-        def __add__(self, other):
-            if isinstance(other, Point):
-                return Point(self.x+other.x, self.y+other.y)
-            else:
-                raise TypeError("Points can only be added to other points")
+    def __eq__(self, other):
+        EPS = 1e-6
 
-        def __mul__(self, other):
-            if isinstance(other, Number):
-                return Point(self.x*other, self.y*other)
-            else:
-                raise TypeError("Points can only multiplied by numbers")
+        return abs(self.x - other[0]) < EPS \
+           and abs(self.y - other[1]) < EPS
 
-        def __rmul__(self, *args, **kwds):
-            return self.__mul__(*args, **kwds)
+    def __ne__(self, other):
+        return not (self == other)
 
-        def __eq__(self, other):
-            EPS = 1e-6
+    def __len__(self):
+        return 2
 
-            return abs(self.x - other[0]) < EPS \
-               and abs(self.y - other[1]) < EPS
+    def __getitem__(self, idx):
+        if idx == 0:
+            return self.x
+        elif idx == 1:
+            return self.y
+        else:
+            raise IndexError()
 
-        def __len__(self):
-            return 2
+    def __sub__(self, other):
+        if isinstance(other, Point):
+            return Point(self.x-other.x, self.y-other.y)
+        else:
+            raise TypeError("Points can only be subtracted from other points")
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+class Size(Point):
+    def __init__(self, w, h):
+        super(Size, self).__init__(w, h)
     
-        def __getitem__(self, idx):
-            if idx == 0:
-                return self.x
-            elif idx == 1:
-                return self.y
-            else:
-                raise IndexError()
+    def GetWidth(self):  return self.x
+    def GetHeight(self): return self.y
 
-        def __sub__(self, other):
-            if isinstance(other, Point):
-                return Point(self.x-other.x, self.y-other.y)
-            else:
-                raise TypeError("Points can only be subtracted from other points")
+    def __getattr__(self, name):
+        if name == "width": return self.x
+        elif name == "height": return self.y
+        else:
+            raise AttributeError()
 
-        def __hash__(self):
-            return hash((self.x, self.y))
+class Color():
+    def __init__(self, red=0, green=0, blue=0):
+        self.color = (red, green, blue)
 
-    class Size(Point):
-        def __init__(self, w, h):
-            super(Size, self).__init__(w, h)
-        
-        def GetWidth(self):  return self.x
-        def GetHeight(self): return self.y
+    def __getitem__(self, idx):
+        return self.color[idx]
+    
+    def Red(self):   return self.color[0]
+    def Green(self): return self.color[1]
+    def Blue(self):  return self.color[2]
 
-        def __getattr__(self, name):
-            if name == "width": return self.x
-            elif name == "height": return self.y
-            else:
-                raise AttributeError()
+    def SetFromName(self, name):
+        colorMapping = {'RED': (255,0,0),
+                        'ORANGE': (255,128,0),
+                        'YELLOW': (255,255,0),
+                        'GREEN': (0,255,0),
+                        'BLUE': (0,0,255),
+                        'PURPLE': (255,0,255),
+                        'WHITE': (255,255,255),
+                        'BLACK': (0,0,0)}
 
-    class Color():
-        def __init__(self, red=0, green=0, blue=0):
-            self.color = (red, green, blue)
-
-        def __getitem__(self, idx):
-            return self.color[idx]
-        
-        def Red(self):   return self.color[0]
-        def Green(self): return self.color[1]
-        def Blue(self):  return self.color[2]
-
-        def SetFromName(self, name):
-            colorMapping = {'RED': (255,0,0),
-                            'ORANGE': (255,128,0),
-                            'YELLOW': (255,255,0),
-                            'GREEN': (0,255,0),
-                            'BLUE': (0,0,255),
-                            'PURPLE': (255,0,255),
-                            'WHITE': (255,255,255),
-                            'BLACK': (0,0,0)}
-
-            if name.upper() in colorMapping:
-                self.color = colorMapping[name.upper()]
-            else:
-                raise NotImplementedError("Color '%s' not recognized" % name)
+        if name.upper() in colorMapping:
+            self.color = colorMapping[name.upper()]
+        else:
+            raise NotImplementedError("Color '%s' not recognized" % name)
             
-                
-
 ############################################################
 
 # Direction IDs (for defining convexity):
@@ -152,7 +143,7 @@ class prettierJSONEncoder(json.JSONEncoder):
             return super(prettierJSONEncoder, self)._newline_indent()
 
 
-class RegionFileInterface:
+class RegionFileInterface(object):
     """
     A wrapper class for handling collections of regions and associated metadata.
 
@@ -237,19 +228,13 @@ class RegionFileInterface:
         COLLINEAR_TOLERANCE = 1  # pixel
 
         # Iterate over faces in obj1
-        for face in obj1.getFaces():
-            pta = Point(*face[0])
-            ptb = Point(*face[1])
-
+        for pta, ptb in obj1.getFaces():
             # Since we are modifying obj2, it's easier to re-run the loop than keep track of indices
             # in the case that we are adding two points.  Lazy, yes, but effective for now.
             clean = False
             while not clean:
                 clean = True
-                for other_face in obj2.getFaces():
-                    other_pta = Point(*other_face[0])
-                    other_ptb = Point(*other_face[1])
-
+                for other_pta, other_ptb in obj2.getFaces():
                     [on_segment_a, d_a, pint_a] = pointLineIntersection(other_pta, other_ptb, pta)
                     [on_segment_b, d_b, pint_b] = pointLineIntersection(other_pta, other_ptb, ptb)
                     if d_a < COLLINEAR_TOLERANCE and d_b < COLLINEAR_TOLERANCE: # Check for collinearity
@@ -280,7 +265,7 @@ class RegionFileInterface:
         Returns a list of shared faces
         """
 
-        # Calculate adjoining faces:
+        # Create empty adjacency matrix
         self.transitions = [[[] for j in range(len(self.regions))] for i in range(len(self.regions))]
 
         transitionFaces = {} # This is just a list of faces to draw dotted lines on
@@ -374,9 +359,7 @@ class RegionFileInterface:
             for region2, faces in enumerate(destinations[region1+1:]):
                 if faces == []: continue    # No transition between these regions, so skip
 
-                faceData = []
-                for face in faces:
-                    faceData.extend([face[0][0], face[0][1], face[1][0], face[1][1]])
+                faceData = [coord for face in faces for pt in face for coord in pt]
 
                 transitionData.append("\t".join([self.regions[region1].name,
                                                  self.regions[region1 + 1 + region2].name] +
@@ -450,7 +433,7 @@ class RegionFileInterface:
             for i in range(2, len(transData), 4):
                 p1 = Point(float(transData[i]), float(transData[i+1]))
                 p2 = Point(float(transData[i+2]), float(transData[i+3]))
-                faces.append(tuple(sorted((p1, p2))))
+                faces.append(frozenset((p1, p2)))
                 
             # During adjacency matrix reconstruction, we'll mirror over the diagonal
             self.transitions[region1][region2] = faces
@@ -471,7 +454,7 @@ class RegionFileInterface:
    
 ############################################################
  
-class Region:
+class Region(object):
     """ A rectangular or polygonal region, defined by the following properties:
 
             - 'name'          Region name
@@ -505,6 +488,7 @@ class Region:
         self.alignmentPoints   = [False] * len([x for x in self.getPoints()])
         self.isObstacle = False
         self.holeList = []
+
     # =================================
     # == Region Manipulation Methods ==
     # =================================
@@ -700,43 +684,40 @@ class Region:
     def getFaces(self,includeHole=False):
         """
         Wrapper function to allow for iteration over faces of regions.
-        A face is a tuple of the two points (in absolute coordinates) that make up the face,
-        sorted so that a given face is defined uniquely.
-
-        FIXME: Make sure we take advantage of this uniqueness elsewhere; I think we check
-        too many conditions sometimes
+        A face is a frozenset of the two points (in absolute coordinates) that make up the face.
         """
 
         lastPt = None
         for pt in self.getPoints():
 
-            thisPt = copy.deepcopy(pt)
+            thisPt = pt
 
-            if lastPt != None:
-                yield tuple(sorted((lastPt, thisPt)))
+            if lastPt is not None:
+                yield frozenset((lastPt, thisPt))
             else:
                 firstPt = thisPt
 
             lastPt = thisPt
 
-        yield tuple(sorted((lastPt, firstPt))) # Closing face
+        yield frozenset((lastPt, firstPt)) # Closing face
+
         # also include edges of holes in the get faces for checking adjancency
         if includeHole:
-            for i,hole in enumerate(self.holeList):
+            for i, hole in enumerate(self.holeList):
+
                 lastPt = None
                 for pt in self.getPoints(hole_id=i):
-                    thisPt = copy.deepcopy(pt)
 
-                    if lastPt != None:
-                        yield tuple(sorted((lastPt, thisPt)))
+                    thisPt = pt
+
+                    if lastPt is not None:
+                        yield frozenset((lastPt, thisPt))
                     else:
                         firstPt = thisPt
 
                     lastPt = thisPt
 
-                yield tuple(sorted((lastPt, firstPt))) # Closing face
-                
-
+                yield frozenset((lastPt, firstPt)) # Closing face
 
     def getPoints(self, relative=False, hole_id=None):
         """

src/regionEditor.py
@@ -374,14 +374,13 @@ class DrawingFrame(wx.Frame):
     
     def testNear(self):
         newReg = self.selection[0].findRegionNear(20, mode='overEstimate')
-        obj = DrawableRegion(newReg.type)
-        obj.setData(newReg.getData())
+        obj = DrawableRegion.fromRegion(newReg)
         self.rfi.regions.append(obj)
         self.drawPanel.Refresh()
 
     def testBetween(self):
         newReg = findRegionBetween(self.selection[0], self.selection[1])
-        obj = DrawableRegion(newReg.type)
+        obj = DrawableRegion.fromRegion(newReg)
         obj.setData(newReg.getData())
         self.rfi.regions.append(obj)
         self.drawPanel.Refresh()
@@ -716,8 +715,8 @@ class DrawingFrame(wx.Frame):
                         if obj in self.selection:
                             # TODO: because of the getObjectAt call,
                             # clicks outside the object will not be recognized
-                            for i, face in enumerate(obj.getFaces()):
-                                [on_segment, d, pint] = pointLineIntersection(wx.Point(*face[0]), wx.Point(*face[1]), mousePt)
+                            for i, (face_pta, face_ptb) in enumerate(obj.getFaces()):
+                                [on_segment, d, pint] = pointLineIntersection(face_pta, face_ptb, mousePt)
                                 if on_segment and d <= 4:
                                     self._saveUndoInfo()
                                     obj.addPoint(pint-obj.position, i+1)
@@ -734,7 +733,7 @@ class DrawingFrame(wx.Frame):
 
                     for obj in self.rfi.regions:
                         topLeft = obj.position
-                        botRight = obj.position + wx.Point(obj.size.GetWidth(), obj.size.GetHeight())
+                        botRight = obj.position + Point(obj.size.GetWidth(), obj.size.GetHeight())
                             
                         # First do rough checking based on obj bound rects
                         if (topLeft.x > mousePt.x + SNAP_RADIUS or botRight.x < mousePt.x - SNAP_RADIUS or
@@ -807,7 +806,7 @@ class DrawingFrame(wx.Frame):
                 if self.mouseMode == mouse_RESIZE and len(self.selection) != 0 and self.selection[0] == obj: continue
 
                 topLeft = obj.position
-                botRight = obj.position + wx.Point(obj.size.GetWidth(), obj.size.GetHeight())
+                botRight = obj.position + Point(obj.size.GetWidth(), obj.size.GetHeight())
                     
                 # First do rough checking based on obj bound rects
                 if (topLeft.x > self.curPt.x + SNAP_RADIUS or botRight.x < self.curPt.x - SNAP_RADIUS or
@@ -915,8 +914,8 @@ class DrawingFrame(wx.Frame):
                 textWidth, textHeight = dc.GetTextExtent(labelText)
                 
                 # TODO: Better text placement algorithm for concave polygons?
-                dc.SetBrush(wx.Brush(obj.color, wx.SOLID))
-                dc.SetPen(wx.Pen(obj.color, 1, wx.SOLID))
+                dc.SetBrush(wx.Brush(wx.Colour(*obj.color), wx.SOLID))
+                dc.SetPen(wx.Pen(wx.Colour(*obj.color), 1, wx.SOLID))
                 center = obj.getCenter()
                 if obj.name.lower() == "boundary":
                     textX = obj.position.x
@@ -1022,6 +1021,7 @@ class DrawingFrame(wx.Frame):
         """
 
         self.transitionFaces = self.rfi.recalcAdjacency() # This is just a list of faces to draw dotted lines on
+        #self.transitionFaces = dict((face, None) for face in self.rfi.getExternalFaces())
 
         #self.drawPanel.Refresh()
 
@@ -1425,8 +1425,8 @@ class DrawingFrame(wx.Frame):
         """
         self._saveUndoInfo()
 
-        obj = DrawableRegion(reg_RECT, position=wx.Point(x, y),
-                            size=wx.Size(width, height))
+        obj = DrawableRegion(reg_RECT, position=Point(x, y),
+                            size=Size(width, height))
         self.rfi.setToDefaultName(obj)
         self.checkSubfaces(obj)
         self.rfi.regions.insert(0, obj)
@@ -1505,8 +1505,7 @@ class DrawingFrame(wx.Frame):
 
         # Convert from Regions to DrawableRegions
         for i, region in enumerate(self.rfi.regions):
-            obj = DrawableRegion(region.type)
-            obj.setData(region.getData())
+            obj = DrawableRegion.fromRegion(region)
             self.rfi.regions[i] = obj
             del region
 
@@ -1665,10 +1664,10 @@ class DrawingFrame(wx.Frame):
 
             obj.recalcBoundingBox()
         else:
-            topLeft  = wx.Point(min(anchorPt.x, newPt.x),
-                               min(anchorPt.y, newPt.y))
-            botRight = wx.Point(max(anchorPt.x, newPt.x),
-                               max(anchorPt.y, newPt.y))
+            topLeft  = Point(min(anchorPt.x, newPt.x),
+                             min(anchorPt.y, newPt.y))
+            botRight = Point(max(anchorPt.x, newPt.x),
+                             max(anchorPt.y, newPt.y))
 
             newWidth  = botRight.x - topLeft.x
             newHeight = botRight.y - topLeft.y
@@ -1676,7 +1675,7 @@ class DrawingFrame(wx.Frame):
             # Finally, adjust the bounds of the object to match the new dimensions.
     
             obj.position = topLeft
-            obj.size = wx.Size(botRight.x - topLeft.x, botRight.y - topLeft.y)
+            obj.size = Size(botRight.x - topLeft.x, botRight.y - topLeft.y)
 
         self.checkSubfaces(obj)
 
@@ -1692,10 +1691,7 @@ class DrawingFrame(wx.Frame):
         self._saveUndoInfo()
 
         for obj in self.selection:
-            pos = obj.position
-            pos.x = pos.x + offsetX
-            pos.y = pos.y + offsetY
-            obj.position = pos
+            obj.position += Point(offsetX, offsetY)
 
         self.drawPanel.Refresh()
         self.dirty = True
@@ -1750,9 +1746,7 @@ class DrawingFrame(wx.Frame):
                 # Special case for boundary: only react to clicks on perimeter
 
                 # Check faces
-                for face in obj.getFaces():
-                    pta = wx.Point(*face[0])
-                    ptb = wx.Point(*face[1])
+                for pta, ptb in obj.getFaces():
                     [on_segment, d, pint] = pointLineIntersection(pta, ptb, pt)
                     if on_segment and d <= BOUNDARY_TOLERANCE:
                         return obj
@@ -1787,7 +1781,7 @@ class DrawingFrame(wx.Frame):
             size     = self.selection[i].size
     
             if self.selection[i].type == reg_POLY:
-                dc.DrawLines(self.selection[i].pointArray + [self.selection[i].pointArray[0]],
+                dc.DrawLines([wx.Point(p.x, p.y) for p in self.selection[i].pointArray + [self.selection[i].pointArray[0]]],
                              position.x + offsetX, position.y + offsetY)
             else:
                 dc.DrawRectangle(position.x + offsetX, position.y + offsetY,
@@ -1980,14 +1974,14 @@ class EditRegionDialog(wx.Dialog):
         """
         self.textCtrl.SetValue(obj.name)
         self.textCtrl.SetSelection(0, len(obj.name))
-        self.colorPicker.SetColour(obj.color)
+        self.colorPicker.SetColour(wx.Colour(*obj.color))
         self.checkbox_obstacle.SetValue(obj.isObstacle)
 
     def dialogToObject(self, obj):
         """ Copy the properties from the dialog box into the given text object.
         """
         obj.name = self.textCtrl.GetValue()
-        obj.color = self.colorPicker.GetColour()
+        obj.color = Color(*self.colorPicker.GetColour())
         obj.isObstacle = self.checkbox_obstacle.GetValue()
 
 #----------------------------------------------------------------------------