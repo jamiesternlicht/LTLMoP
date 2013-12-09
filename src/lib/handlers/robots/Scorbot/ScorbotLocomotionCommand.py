68d268b91237a70beb6fcd53e7f0534e75ddecc6
jamiesternlicht@gmail.com <jamiesternlicht@gmail.com>
2013-12-09 17:06:34 -0500

    Added readlines to complete serial port writes, added logging statements

--- src/lib/handlers/robots/Scorbot/ScorbotLocomotionCommand.py
+++ src/lib/handlers/robots/Scorbot/ScorbotLocomotionCommand.py
@@ -5,29 +5,29 @@ ScorbotLocomotionCommand.py - The Scorbot's Locomotion Command Handler
 ================================================================================
 """
 
-import time
-import serial
+import time, logging
 
 class locomotionCommandHandler:    
-    """
-    This class will tell the Scorbot which region to move to.
-    """
     
     def __init__(self, proj, shared_data): 
+        """
+        This class will tell the Scorbot which region to move to.
+        """
+
         try: 
             self.scorbotSer = shared_data["ScorbotSer"]
         except: 
-            print "(LOCO) ERROR: No connection to Scorbot"
-            return
+            logging.error("(LOCO) ERROR: No connection to Scorbot")
 
     def sendCommand(self, cmd): 
-        # MAKE COMMANDS 
+        
         try:
             self.scorbotSer.write(cmd) # Writes movement command to serial port.
             # This command comes from ScorbotDrive
+            self.scorbotSer.readline()
+            self.scorbotSer.readline()
         except: 
-            print "(LOCO) ERROR: Didn't perform movement" 
-            return
+            logging.error("(LOCO) ERROR: Didn't perform movement")
 
 
 
